83c285b60f3fcbfafb0ffe6b7e774a4e25d81c54
lfpborba <luisfpb@icloud.com>
2023-02-12 18:59:38 -0300

    feat: implementing file_process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+from ting_file_management.file_management import txt_importer
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_content = txt_importer(path_file)
+    queue_size = len(instance)
+
+    if not file_content:
+        return
+
+    processed_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_content),
+        "linhas_do_arquivo": file_content,
+    }
+
+    for file in range(queue_size):
+        if instance.search(file)["nome_do_arquivo"] == path_file:
+            return None
+
+    instance.enqueue(processed_data)
+    print(processed_data)
 
 
 def remove(instance):
