a6be03f14a5af5ef16cc5e88b6c26b1a01c34120
lfpborba <luisfpb@icloud.com>
2023-02-12 15:52:54 -0300

    feat: implementing file_management.py

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import os
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+
+    # Verifica se a extensão é .txt
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return []
+
+    # Verifica se o arquivo existe
+    if not os.path.exists(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
+
+    with open(path_file, "r") as file:
+        lines = file.read().split("\n")
+
+    return lines
