6580d45e0dc3775c0ede950551615f2228cc95a3
lfpborba <luisfpb@icloud.com>
2023-02-12 22:05:24 -0300

    fix: exist_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search = []
+    word = word.lower()
+    
+    for index in range(len(instance)):
+        file = instance.search(index)
+        line = 0
+        
+        ocurrences = [{"linha": i+1} for i, item in enumerate(file["linhas_do_arquivo"]) if word in item.lower()]
+        
+        if ocurrences:
+            search.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": ocurrences
+            })
+
+    return search
 
 
 def search_by_word(word, instance):
