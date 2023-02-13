b7946102f64b9ebb2a0320f49e947bbb56a0a5ca
lfpborba <luisfpb@icloud.com>
2023-02-12 21:55:39 -0300

    fix: restarting exist_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,26 +1,5 @@
-from ting_file_management.queue import Queue
-
-
-def exists_word(word: str, instance: Queue) -> list:
-    case_insensitive_word = word.lower()
-    words_list = []
-    dict = {}
-    lines = []
-
-    for file in instance.queue:
-        dict['palavra'] = case_insensitive_word
-        dict['arquivo'] = file["nome_do_arquivo"]
-        dict["ocorrencias"] = []
-
-        for i, line in enumerate(file["linhas_do_arquivo"]):
-            if case_insensitive_word in line.lower():
-                word_events = {"linha": i + 1}
-                lines.append(word_events)
-                dict['ocorrencias'] = lines
-        if dict["ocorrencias"]:
-            words_list.append(dict)
-
-    return words_list
+def exists_word(word, instance):
+    """Aqui irá sua implementação"""
 
 
 def search_by_word(word, instance):
